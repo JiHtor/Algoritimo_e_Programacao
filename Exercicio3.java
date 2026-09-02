1eed7f5fa4c65d7f2629c88c0b4e9198842d4abe
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-02 13:30:53 +0000

    Calc

--- Exercicio3.java
+++ Exercicio3.java
@@ -1,5 +1,7 @@
-public class Exercicio3 {
-    public static void main (String [] args){
-        
-    }
-}
+public class Exercicio3 { 
+public static void main(String[] args) {
+int numero = 5; for (int i = 1; i <= 10; i++) { 
+System.out.printf("%d x %d = %d%n", numero, i, numero * i); 
+} 
+} 
+} 
