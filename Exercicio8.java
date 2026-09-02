f4204dbb5a86c721649877d84b7bf2fcc08911de
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-02 14:38:34 +0000

    exerc

---
+++ Exercicio8.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio8 {
+    public static void main(String [] args){
+        Scanner entrada = new Scanner(System.in);
+
+        double media = (8 + 7.5 + 4.5 + 9) / 4;
+
+        System.out.println("A media das notas é " + media);
+
+        entrada.close();
+    }
+}
