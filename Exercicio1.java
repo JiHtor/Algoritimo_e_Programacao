b9c9ecebe14406ee538f680e4e7849aed50766a7
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-02 13:11:41 +0000

    exercicios

---
+++ Exercicio1.java
@@ -0,0 +1,10 @@
+public class Exercicio1 {
+    public static void main (String [] args){
+        String nome = "Heitor Soares da Silva";
+        int RA = 1143549033;
+        String curso = "Técnologia em Análise e Desenvolvimento de Sistemas";
+        System.out.println("Nome = "+ nome);
+        System.out.println("RA = "+ RA);
+        System.out.println("Curso = " + curso);
+    }
+}
---
+++ Exercicio2.java
@@ -0,0 +1,11 @@
+public class Exercicio2 {
+    public static void main (String [] args){
+        int idade = 19;
+        double altura = 1.81;
+        String status = "Presente";
+        System.out.println("Idade: "+ idade);
+        System.out.println("Altura: "+ altura);
+        System.out.println("Status: "+ status);
+    }
+    
+}
