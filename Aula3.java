a7b4b71f16a49cd7fcf6074fac668ac11c8070c1
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-02 12:46:05 +0000

    Exercicios

---
+++ Aula3.java
@@ -0,0 +1,8 @@
+public class Aula3 {
+    public static void main (String [] args){
+        System.out.println("Ola Mundo");
+        System.out.println("Minha idade é: "+ 19 +" anos.");
+        System.out.println("O resultado é: " + (3 + 5 <= 22));
+    }
+
+}
