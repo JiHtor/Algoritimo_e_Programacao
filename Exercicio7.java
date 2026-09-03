f615e5ab8ddf9fcdff5a8bdd5dcc8fac69d0c8af
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-03 00:56:23 +0000

    exerc

--- Exercicio7.java
+++ Exercicio7.java
@@ -1,8 +1,16 @@
-import java.util.Scanner;
-
 public class Exercicio7 {
     public static void main (String [] agrs){
-        Scanner entrada = new Scanner(System.in);
+        boolean p1 = (120 - 30) == (Math.pow(3,30));
+        boolean p2 = (!((20 % 4) == 1) || (9 != 9));
+        boolean p3 = ((5 % 2) >3);
+        boolean p4 = ("a" == "A");
+
+
+        System.out.println("O resultado é "+p1);
+        System.out.println("O resultado é "+p2);
+        System.out.println("O resultado é "+p3);
+        System.out.println("O resultado é "+p4);
+        
 
         
 
