278f81d7687ab9b1c06f966a28559ac4c9c808f1
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-09 14:23:48 +0000

    ex

--- Ex3.java
+++ Ex3.java
@@ -5,16 +5,23 @@ public class Ex3 {
 
         Scanner entrada = new Scanner(System.in);
 
+        int diferenca;
+
         System.out.println("Digite o primeiro numero");
         int num1 = entrada.nextInt();
-        System.out.println("Digite o primeiro numero");
+        System.out.println("Digite o segundo numero");
         int num2 = entrada.nextInt();
 
         if(num1 == num2){
             System.out.println("Os numeros são iguais");
-        }else if () {
-            
+        }else if (num1 > num2) {
+            diferenca = num1 - num2;
+             System.out.println("O Numero 1 > 2, e a subtração deles é "+ diferenca);         
+        }else if(num1 < num2 ){
+            diferenca = num2 - num1;
+            System.out.println("O Numero 1 < 2, e a subtração deles é "+ diferenca);         
         }
+        entrada.close();
 
 
 
