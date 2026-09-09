f5079a06af0645760b6f14fcf908875ed0ffd75b
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-09 18:47:50 +0000

    exerc

---
+++ Ex6.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Ex6{
+    public static void main(String[] args){
+        Scanner entrada = new Scanner(System.in);
+
+        Double peso;
+        System.out.println("Digite seu sexo.");
+        String sexo = entrada.nextLine();
+
+        System.out.println("Digite sua altura.");
+        Double altura = entrada.nextDouble();
+
+        if(sexo.equalsIgnoreCase("Masculino")) {
+            peso = (72.7 * altura) - 58;   
+            System.out.println("Seu peso ideal é "+ peso);
+        }else if (sexo.equalsIgnoreCase("Feminino")){
+            peso = (62.1 * altura) - 44.7; 
+            System.out.println("Seu peso ideal é "+ peso);
+        }else {
+            System.out.println("Sexo não identificado.");
+        }
+    }
+}
+
