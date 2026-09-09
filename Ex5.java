0648d21915df5d48ac3b1b89444a7ca58e079c31
Heitor_jih <heitor.ssilva7@senacsp.edu.br>
2026-09-09 14:54:25 +0000

    xe

---
+++ Ex5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Digite o numero");
+        int num = entrada.nextInt();
+
+        if(50 >= num <= 100){
+        System.out.println("Pertence ao intervalo");
+        }else{
+             System.out.println("Não pertence ao intervalo");
+        }
+    }
+}
